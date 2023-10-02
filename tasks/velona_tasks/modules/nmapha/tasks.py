f805303a309e461853cc03da34ef9fd495962a14
Christophe Olinger <christophe@olinger.eu>
2023-10-02 11:51:37 +0200

    Test nmap

--- tasks/velona_tasks/modules/nmapha/tasks.py
+++ tasks/velona_tasks/modules/nmapha/tasks.py
@@ -10,8 +10,13 @@ from ... import config
 def execute_nmap_ha():
     
     session = Pg_session()
+    import nmap3
     time.sleep(2)
     print(session)
     print("DONE")
-    a = 1/0
+    
+    nmap = nmap3.Nmap()
+    results = nmap.scan_top_ports("192.168.178.20-130")
+    print(results)
+
     return "Import successful"
