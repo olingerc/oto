7cb4125d3d1d2fa47add934b24ed8deebc48a7f5
Christophe Olinger <christophe@olinger.eu>
2023-10-07 10:53:19 +0200

    corect

--- tasks/velona_tasks/modules/nmapha/tasks.py
+++ tasks/velona_tasks/modules/nmapha/tasks.py
@@ -164,12 +164,12 @@ def list_results():
             if len(d.intervals) > 10:
                 scans_by_ip[d.ipaddress] = {
                     "state": d.state,
-                    "intervals": len(d.intervals[-10:])
+                    "intervals": d.intervals[:-10]
                 }
             else:
                 scans_by_ip[d.ipaddress] = {
                     "state": d.state,
-                    "intervals": len(d.intervals)
+                    "intervals": d.intervals
                 }
         
         res = []
